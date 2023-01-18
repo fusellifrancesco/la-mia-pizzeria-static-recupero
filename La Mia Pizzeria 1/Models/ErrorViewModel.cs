d8a2573bb8792d67835dac3ba89fdb3290e2dfe5
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-18 16:28:21 +0100

    Add project files.

---
+++ La Mia Pizzeria 1/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace La_Mia_Pizzeria_1.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
